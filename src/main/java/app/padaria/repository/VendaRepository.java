22baa5da9cd81cea0977b225e867dbf6df304a5a
Murilo Eberhardt <murilo.05eberhardt@gmail.com>
2025-08-17 22:38:50 -0300

    Criação do VendaRepository

---
+++ src/main/java/app/padaria/repository/VendaRepository.java
@@ -0,0 +1,13 @@
+package app.padaria.repository;
+
+import app.padaria.model.Venda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface VendaRepository extends JpaRepository<Venda, Long> {
+
+    List<Venda> findByDataVendaBetween(LocalDateTime inicio, LocalDateTime fim);
+
+    List<Venda> findByClienteId(Long clienteId);
+}
