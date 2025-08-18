05126f527d5cb9d3b8edc10449ccee4c9b47bede
Murilo Eberhardt <murilo.05eberhardt@gmail.com>
2025-08-17 22:51:06 -0300

    Atualização do PadariaApp

--- src/main/java/app/padaria/PadariaApplication.java
+++ src/main/java/app/padaria/PadariaApplication.java
@@ -1,4 +1,12 @@
 package app.padaria;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PadariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PadariaApplication.class, args);
+    }
 }
